942a37a155fee9cb8988848b5a4ec2f0513b48ac
Declan Reid <2794@sakyabram.vic.edu.au>
2024-12-21 04:26:47 +0000

    Version 1.0.0 ported to new template.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,65 @@
 #include <appdef.hpp>
-#include <stdio.h>
-#include <stdlib.h>
+#include <sdk/calc/calc.hpp>
+#include <sdk/os/input.hpp>
+#include <sdk/os/lcd.hpp>
 
 /*
  * Fill this section in with some information about your app.
  * All fields are optional - so if you don't need one, take it out.
  */
-APP_NAME("STDLIB test")
-APP_DESCRIPTION("Testing STDLIB")
-APP_AUTHOR("PC")
-APP_VERSION("1.0.2")
+APP_NAME("Doodlepad")
+APP_DESCRIPTION("A drawing app")
+APP_AUTHOR("Decky Boiii")
+APP_VERSION("1.0.0")
 
-extern "C" int __attribute__((section(".bootstrap.text"))) main(void) {
-  fputs("Loading the CP test\n", stdout);
-  exit(EXIT_SUCCESS);
+extern "C" void main() {
+  calcInit(); // backup screen and init some variables
+
+  LCD_ClearScreen();
+  LCD_Refresh();
+
+  struct InputEvent event;
+  int old_p1_x, old_p1_y;
+
+  old_p1_x = 0;
+  old_p1_y = 0;
+
+  while (true) {
+
+    uint32_t key1, key2;  // First create variables
+    getKey(&key1, &key2); // then read the keys
+    if (testKey(
+            key1, key2,
+            KEY_CLEAR)) { // Use testKey() to test if a specific key is pressed
+      break;
+    }
+    if (testKey(key1, key2, KEY_BACKSPACE)) {
+      LCD_ClearScreen();
+      LCD_Refresh();
+    }
+
+    GetInput(&event, 0xFFFFFFFF, 0x10);
+
+    switch (event.type) {
+    case EVENT_TOUCH:
+      if (event.data.touch_single.direction == TOUCH_DOWN) {
+        setPixel(event.data.touch_single.p1_x, event.data.touch_single.p1_y,
+                 RGB_TO_RGB565(0, 0, 0));
+        old_p1_x = event.data.touch_single.p1_x;
+        old_p1_y = event.data.touch_single.p1_y;
+      }
+
+      if (event.data.touch_single.direction == TOUCH_HOLD_DRAG) {
+        line(old_p1_x, old_p1_y, event.data.touch_single.p1_x,
+             event.data.touch_single.p1_y, RGB_TO_RGB565(0, 0, 0));
+        old_p1_x = event.data.touch_single.p1_x;
+        old_p1_y = event.data.touch_single.p1_y;
+      }
+
+      LCD_Refresh();
+      break;
+    }
+  }
+
+  calcEnd(); // restore screen and do stuff
 }
